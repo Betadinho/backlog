3247ab83122622de048cf93ee154203f387363c1
betadinho <alphamarioprechtl@gmail.com>
2021-02-05 01:49:57 +0100

    Add means to create new projects and tasks

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,15 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-});
-
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line no-unused-vars
 function toggle_task_details(coll) { 
-    coll.classList.toggle("active");
-    var content = coll.closest('.task-container').getElementsByClassName('expandable-content')[0]
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-    }
-
-
+	coll.classList.toggle("active")
+	var content = coll.closest(".task-container").getElementsByClassName("expandable-content")[0]
+	if (content.style.maxHeight) {
+		content.style.maxHeight = null
+	} else {
+		content.style.maxHeight = content.scrollHeight + "px"
+	}
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+})
+
     
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,75 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {
+/* eslint-disable linebreak-style */
+"use strict"
+function modalToggle(modal, html) {
+	html[0].classList.add("is-clipped")
+	modal.classList.add("is-active")
+	//document.getElementById('signup-first-field').focus();
+
+	//Toggle modal off when close (X in top right corner) or cancel button is clicked
+	let modal_close = modal.querySelectorAll(".modal-close, .cancel")
+	for (const el of modal_close) {
+		el.addEventListener("click", () => {
+			html[0].classList.remove("is-clipped")
+			modal.classList.remove("is-active")
+		})
+	}
+}
+document.addEventListener("DOMContentLoaded", () => {
 	//Bulma Navbar-Menu Mobile Toggle
 	// Get all "navbar-burger" elements
-	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0)
 
-  	// Check if there are any navbar burgers
+	// Check if there are any navbar burgers
 	if ($navbarBurgers.length > 0) {
 
-	    // Add a click event on each of them
-	    $navbarBurgers.forEach( el => {
-	  		el.addEventListener('click', () => {
+		// Add a click event on each of them
+		$navbarBurgers.forEach( el => {
+			el.addEventListener("click", () => {
 
-	        // Get the target from the "data-target" attribute
-	        const target = el.dataset.target;
-	        const $target = document.getElementById(target);
+				// Get the target from the "data-target" attribute
+				const target = el.dataset.target
+				const $target = document.getElementById(target)
 
-	        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-	        el.classList.toggle('is-active');
-	        $target.classList.toggle('is-active');
-  			});
-	    });
-    } 	// <-- Navbarmenu togle end
+				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+				el.classList.toggle("is-active")
+				$target.classList.toggle("is-active")
+			})
+		})
+	} 	// <-- Navbarmenu togle end
 
 
-	let html = document.getElementsByTagName('html');
-	let signup_button = document.getElementById('signup-button');
-	let signup_modal = document.getElementById('signup-modal');
+	const html = document.getElementsByTagName("html")
+    
+	const signup_button = document.getElementById("signup-button")
+	const signup_modal = document.getElementById("signup-modal")
+    
+	const project_create_button = document.getElementById("project-create-button")
+	const project_modal = document.getElementById("project-create-modal")
 
-	let login_button = document.getElementById('login-button');
-	let login_modal = document.getElementById('login-modal');
-    //let logout_button = document.getElementsByClassName('logout-button');
-    //let logout_modal = document.getElementsByClassName('logout-modal');
+	const task_create_button = document.getElementById("task-create-button")
+	const task_modal = document.getElementById("task-create-modal")
 
-    //Signup modal toggle
-	signup_button.addEventListener('click', () => {
-		html[0].classList.add('is-clipped');
-        signup_modal.classList.add('is-active');
-        document.getElementById('signup-first-field').focus();
+	const login_button = document.getElementById("login-button")
+	const login_modal = document.getElementById("login-modal")
 
-		//Toggle modal off when close (X in top right corner) or cancel button is clicked
-        var modal_close = signup_modal.querySelectorAll('.modal-close, .cancel-signup');
-        for(e of modal_close) {
-            e.addEventListener('click', () => {
-                html[0].classList.remove('is-clipped');
-                signup_modal.classList.remove('is-active');
-            });
-        };  
-        
-        
-	});
+	const modal_group = [
+		{button:login_button, modal:login_modal},
+		{button:signup_button, modal:signup_modal},
+		{button:project_create_button, modal:project_modal},
+		{button:task_create_button, modal:task_modal}
+	]
 
-	//Login modal toggle
-	login_button.addEventListener('click', () => {
-		//Toggle Modal to be visible
-		html[0].classList.add('is-clipped');
-        login_modal.classList.add('is-active');
-        document.getElementById('login-first-field').focus();
-
-		//Toggle modal off when close is clicked
-		var modal_close = login_modal.querySelectorAll('.modal-close, .cancel-login');
-		for(e of modal_close) {
-            e.addEventListener('click', () => {
-                html[0].classList.remove('is-clipped');
-                login_modal.classList.remove('is-active');
-            });
-        };
-	});
-
-	//logout modal toggle
-	/*logout_button.addEventListener('click', () => {
-		html[0].classList.add('is-clipped');
-		logout_modal.classList.toggle('is-active');
-	});*/
-
-});
+	for(const e of modal_group) {
+		(function() {
+			e.button.addEventListener("click", () => { modalToggle(e.modal, html) })
+		}())
+	}
+})
