f7469422312ada49eabd552cf1e33298d4ed39f6
Betadinho <alphamarioprechtl@gmail.com>
2021-11-14 20:58:00 +0100

    Fix Delete Task Buttons

--- app/static/js/dragHandler.js
+++ app/static/js/dragHandler.js
@@ -1,4 +1,3 @@
-import { persistTaskStageChange } from './crudController.js';
 var dragged;
 var dropzones = document.getElementsByClassName('dropzone');
 const  CSRFTOKEN = () => {
@@ -78,3 +77,82 @@ document.addEventListener('drop', function( event ) {
     
 }, false);
 
+// eslint-disable-next-line no-unused-vars
+function taskController(taskid, cmd, csrf) {
+	//const reload = window.location.href;
+	const headers = new Headers({
+		'X-CSRF-TOKEN': csrf
+	});
+	switch(cmd) {
+	case 'del':
+		fetch(`/delete_task/${taskid}`, {
+			method: 'DELETE',
+			headers,
+		})
+			.then(response => response.json())
+			.then(result => {
+				console.log('Success: ', result);
+			})
+			.catch(error => {
+				console.log('Error: ', error);
+			});
+		window.location.reload();
+		break;
+	case 'edit':
+		//Do stuff
+		break;
+	case 'close':
+		//Do stuff
+		break;
+			
+	}
+}
+
+// eslint-disable-next-line no-unused-vars
+function projectController(projectid, cmd, csrf) {
+	const headers = new Headers({
+		'X-CSRF-TOKEN': csrf
+	});
+	//let reload = window.location.href;
+	switch(cmd) {
+	case 'del':
+		fetch(`/delete_project/${projectid}`, {
+			method: 'DELETE',
+			headers,
+		})
+			.then(response => response.json())
+			.then(result => {
+				//console.log('Result: ', result);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+		window.location.reload();
+		break;
+	case 'edit':
+		//Do stuff
+		break;
+	case 'close':
+		//Do stuff
+		break;
+			
+	}
+}
+
+// eslint-disable-next-line no-unused-vars
+function persistTaskStageChange(taskid, stageid, csrf) {
+	const headers = new Headers({
+		'X-CSRF-TOKEN': csrf,
+	});
+	fetch(`/persiststage?taskid=${taskid}&stageid=${stageid}`, {
+		method: 'POST',
+		headers
+		})
+		.then(response => response.json())
+		.then(result => {
+			console.log('Success: ', result);
+		})
+		.catch(error => {
+			console.log(error);
+		});
+}
